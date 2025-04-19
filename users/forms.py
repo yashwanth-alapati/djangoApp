38c37bee9100647802ec54c72ab5588b663efa99
Yashwanth Alapati <145064639+yashwanth-alapati@users.noreply.github.com>
2025-04-19 13:17:17 -0400

    Add login form

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your username'
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter your password'
+        })
+    )
